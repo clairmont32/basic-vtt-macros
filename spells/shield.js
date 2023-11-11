c58af782453393f0884c211995c0c2522042e58b
Matthew Clairmont <6239004+clairmont32@users.noreply.github.com>
2023-11-11 17:21:54 -0500

    Update shield.js

--- spells/shield.js
+++ spells/shield.js
@@ -5,25 +5,23 @@ const spellName = "Shield";
 // Check if a token is controlled
 if (controlledToken) {
     // Get the actor associated with the token
-     const actor = controlledToken.actor;
-
-
+   const actor = controlledToken.actor;
+  
     // Check if the actor has the specified spell
-    const spell = actor.items.find((item) => item.name === spellName && item.type === "spell");   
+   const spell = actor.items.find((item) => item.name === spellName && item.type === "spell");   
 
-
- if (spell) {
-    let spkr = ChatMessage.getSpeaker({actor: controlledToken})
-    ChatMessage.create({speaker: spkr, content: "Yellow alert", type: CONST.CHAT_MESSAGE_TYPES.EMOTE}, {chatBubble: true});
-
-    // load the sequence
-    new Sequence()
-        .effect()
-        .file("jb2a.shield.01.loop.blue")
-        .atLocation(token)
-        .persist()
-        .scale(.6)
-        .play();
+   if (spell) {
+      let spkr = ChatMessage.getSpeaker({actor: controlledToken})
+      ChatMessage.create({speaker: spkr, content: "Yellow alert", type: CONST.CHAT_MESSAGE_TYPES.EMOTE}, {chatBubble: true});
+  
+      // load the sequence
+      new Sequence()
+          .effect()
+          .file("jb2a.shield.01.loop.blue")
+          .atLocation(token)
+          .persist()
+          .scale(.6)
+          .play();
 
 
     spell.roll()
